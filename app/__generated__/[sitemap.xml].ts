9204a76ec5187c5ec3b419c8556c42e3da616045
chirudeep37 <chirudeep37@gmail.com>
2024-01-28 16:48:43 +0000

    first

---
+++ app/__generated__/[sitemap.xml].ts
@@ -0,0 +1,18 @@
+
+      export const sitemap = {
+  "pages": [
+    {
+      "path": "",
+      "lastModified": "2023-11-17T12:34:06.830Z"
+    },
+    {
+      "path": "/pricing",
+      "lastModified": "2023-11-17T12:34:06.830Z"
+    },
+    {
+      "path": "/about",
+      "lastModified": "2023-11-17T12:34:06.830Z"
+    }
+  ]
+};
+    
---
+++ app/__generated__/_index.server.tsx
@@ -0,0 +1,5 @@
+type Params = Record<string, string | undefined>
+export const loadResources = async (_props: { params: Params }) => {
+return {
+} as Record<string, unknown>
+}
